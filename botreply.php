3933b7d19dbbe430195dae576072320e89391d08
charamp <parinya.peapanom@gmail.com>
2017-01-05 17:37:30 +0700

    m botreply.php

--- botreply.php
+++ botreply.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 				fwrite($myfile, (string) $text[1]);
 				fclose($myfile);
 
-				$suffix = Array( str_repeat('5', rand(3,10)), str_repeat('อิ', rand(3,10)) );
+				$suffix = Array( str_repeat('5', rand(3,10)), str_repeat('อิ', rand(1,2)) );
 				$reply = callApiChat($text[1]) . ' ' . $suffix[rand(0,1)];			
 				$replyToken = $event['source']['roomId'];
 				//$replyToken = $event['replyToken'];
