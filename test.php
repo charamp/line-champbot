9a4e46c4f438bb2c98d997b45fb00a31f5316faf
charamp <parinya.peapanom@gmail.com>
2017-01-05 09:13:35 +0700

    m test.php

--- test.php
+++ test.php
@@ -1,20 +1,22 @@
 <?php
 
-$url = 'http://www.dogdiri.com/chat/dogthink.php';
-$data = array('chattext'=>'สวัสดี');
+function callApiChat($message) {
+	$url = 'http://www.dogdiri.com/chat/dogthink.php';
+	$data = array('chattext'=>'สวัสดี');
 
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+    		'http' => array(
+        		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        		'method'  => 'POST',
+        		'content' => http_build_query($data)
+    			)
+		);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	if ($result === FALSE) { /* Handle error */ }
 
-var_dump($result);
+	echo $result;
+	var_dump($result);
 
 ?>
