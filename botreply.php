617190c54768fd6c04ffaa23473d4fe7aaf2ab72
charamp <parinya.peapanom@gmail.com>
2017-01-05 09:46:23 +0700

    m botreply.php

--- botreply.php
+++ botreply.php
@@ -10,12 +10,20 @@ $replyToken = $lineTargetUID;
 
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
-$text = $event['message']['text'];
 
-$myfile = fopen("aaa.txt", "a") or die("Unable to open file!");
-fwrite($myfile, (string) $text);
-fclose($myfile);
-
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			$myfile = fopen("aaa.txt", "a") or die("Unable to open file!");
+			fwrite($myfile, (string) $text);
+			fclose($myfile);
+		}
+	}
+}
 
 
 /*
