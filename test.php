77f0d541678529452cfa40da63bc9c20d91333af
charamp <parinya.peapanom@gmail.com>
2017-01-05 08:53:06 +0700

    add test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+$url = 'http://www.dogdiri.com/chat/dogthink.php';
+$data = array('chattext'=>'สวัสดี');
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
